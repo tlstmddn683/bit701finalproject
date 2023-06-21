41f51efbc4d343cae263e66935e2a3ad97ccf6bb
tlstmddn683 <tlstmddn683@gmail.com>
2023-06-21 16:20:01 +0900

    commit

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,14 @@
+import photo from './1.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+          <img alt='' src={photo} className="App-logo"  border="0"/>
+          <br/><br/>
+          <h2>도커 배포 성공 기원기</h2>
+    </div>
+  );
+}
+
+export default App;
