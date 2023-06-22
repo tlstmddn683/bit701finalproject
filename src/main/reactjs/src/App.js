18318e43a83d6e5042b0eeccbc7e396fa36218ec
tlstmddn683 <tlstmddn683@gmail.com>
2023-06-22 16:14:48 +0900

    commit !!

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -1,4 +1,5 @@
 import photo from './1.png';
+import photo2 from './2.gif';
 import './App.css';
 
 function App() {
@@ -6,7 +7,10 @@ function App() {
     <div className="App">
           <img alt='' src={photo} className="App-logo"  border="0"/>
           <br/><br/>
+          <img alt='' src={photo2} width={80}/> 
           <h2>도커 배포 성공 기원기</h2>
+          <h2>히오스 갓겜입니다....</h2>
+
     </div>
   );
 }
